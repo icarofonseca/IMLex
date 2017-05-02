88710ef72244b8a7cdb86c4f4b7c51a5d87ad727
Ícaro Fonseca <icaroaragao@outlook.com>
2017-05-02 00:06:51 -0300

    Correcting number format

--- fin_SVM2.m
+++ fin_SVM2.m
@@ -4,4 +4,4 @@ x = [1 0; 0 1; 0 -1; -1 0; 0 2; 0 -2; -2 0];
 y = [-1; -1; -1; 1; 1; 1; 1];
 
 %libsvm command
-model = svmtrain(y, x, '-t 1 -d 2 -g 1 -r 1 -c 10^10');
+model = svmtrain(y, x, '-t 1 -d 2 -g 1 -r 1 -c 1e+10');
